afb1d9355d22b1d5a10accba964d370f545a76dd
sivaramaaa <sivaramaaa@gmail.com>
2017-03-11 23:11:36 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,6 +4,8 @@ chown root device-plug.sh
 chgrp root device-plug.sh
 
 cp 100-sample.rules /etc/udev/rules.d
+cp device-plug.sh  /usr/local/bin
+cp device-unplug.sh  /usr/local/bin
 sudo service udev restart 
 
 cat lockdown.sh > /etc/rc.local
