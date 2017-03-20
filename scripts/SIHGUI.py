0b912ccf21afbdee476c520b1ef48b8155410083
sivaramaaa <sivaramaaa@gmail.com>
2017-03-20 08:25:39 +0530

    Update SIHGUI.py

--- scripts/SIHGUI.py
+++ scripts/SIHGUI.py
@@ -11,12 +11,14 @@ while(1):
 
 os.popen("zenity --warning --title='Software Limitation' --text='The Software supports only single file copy feature for now. Updates arrive soon! Click OK to continue to the copy wizard'")
 cpysrc = os.popen('zenity --file-selection --multiple --title="Select the source location"').read().strip()
-os.popen("cd "+cpysrc)
+ #os.popen("cd "+cpysrc) <-- no need of this as we arge giving absolute path 
+    
 cpydes = os.popen('zenity --file-selection --directory --title="Select the destination location"').read().strip()
-
+cmd1 = "echo "+passw+" | "+"gpg -c --passphrase-fd 0  -o "+cpydes+"/cryp"+" "+cpysrc
+cmd2 = "echo "+passw+" | "+"gpg --passphrase-fd  -o "+cpydes+"/cryp"+" "+cpysrc
 if "/Desktop" in cpydes:
-    os.popen("gpg -c --passphrase "+passw+" -o "+cpydes+"/cryp"+" "+cpysrc)
+    os.popen(cmd1)
 if "/Desktop" in cpysrc:
-    os.popen("gpg --passphrase "+passw+" -o "+cpydes+"/cryp"+" "+cpysrc)
+    os.popen(cmd2)
 os.popen("zenity --title='Success' --info --text='The requested file has been securely copied successfully!'")
 
